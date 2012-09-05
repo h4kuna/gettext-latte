082c58c1e9d64aaf0cfefbd4a393a51fc3f9dd56
milan <milan.matejcek@gmail.com>
2012-09-05 12:46:05 +0200

    fake support

--- TranslatorFake.php
+++ TranslatorFake.php
@@ -7,7 +7,11 @@ use Nette\Object,
 
 class TranslatorFake extends Object implements ITranslator {
 
-    function translate($message, $count = null) {
+    function translate($message, $count = NULL) {
+        return call_user_func_array('self::t', func_get_args());
+    }
+
+    static function t($message /*, ...*/) {
         $args = func_get_args();
         array_shift($args);
         return ($args) ? vsprintf($message, $args) : $message;
