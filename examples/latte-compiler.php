43b60e0efcba4fde27357517e8bfb86c791e0889
Milan Matejcek <milan.matejcek@marketvision.cz>
2014-08-21 13:15:46 +0200

    Typo.

---
+++ examples/latte-compiler.php
@@ -0,0 +1,16 @@
+<?php
+
+use h4kuna\Gettext\Latte\MagicObject;
+
+$container = require_once __DIR__ . '/bootstrap.php';
+
+/* @var $compiler h4kuna\Gettext\Latte\LatteCompiler */
+$compiler = $container->getService('gettextLatteExtension.compiler');
+$compiler->addInclude(__DIR__ . '/../examples');
+$compiler->addExclude(__DIR__ . '/../examples/example.latte');
+$compiler->addInclude(__DIR__ . '/../examples');
+
+// undefoned property
+$template = $compiler->getTemplate();
+$template->languages = $template->mail = $template->menu = new MagicObject(array());
+$compiler->run();
