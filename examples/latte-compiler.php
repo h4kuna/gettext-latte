d46539ae01695ee5fa8c3642cda2a187f0779f9f
Milan Matejcek <milan.matejcek@marketvision.cz>
2014-08-21 13:22:06 +0200

    Typo.

--- examples/latte-compiler.php
+++ examples/latte-compiler.php
@@ -5,7 +5,7 @@ use h4kuna\Gettext\Latte\MagicObject;
 $container = require_once __DIR__ . '/bootstrap.php';
 
 /* @var $compiler h4kuna\Gettext\Latte\LatteCompiler */
-$compiler = $container->getService('gettextLatteExtension.compiler');
+$compiler = $container->getByType('h4kuna\Gettext\Latte\LatteCompiler');
 $compiler->addInclude(__DIR__ . '/../examples');
 $compiler->addExclude(__DIR__ . '/../examples/example.latte');
 $compiler->addInclude(__DIR__ . '/../examples');
